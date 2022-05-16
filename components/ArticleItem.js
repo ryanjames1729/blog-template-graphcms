5eeb2984a98404008aa9709642b909d4634bfae3
rjames <rjames@rjames-mbpr-us.cdschool.org>
2022-05-16 14:20:42 -0400

    initial commit

---
+++ components/ArticleItem.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import articleStyles from '../styles/Article.module.css';
+
+const ArticleItem = ({article}) => {
+    return (
+        <div>
+            <Link href="/article/[id]" as={`/article/${article.slug}`}>
+                <div className={articleStyles.card}>
+                    <h3>{article.title} &rarr;</h3>
+                    <p>{article.desc.text}</p>
+                </div>
+            </Link>
+        </div>
+    )
+}
+
+export default ArticleItem
